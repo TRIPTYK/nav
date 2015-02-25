5a6eaaee5655bde9a0dc325721e0e9e4d8f5a4b5
TheDigitalCrazyPainter <gilles@triptyk.eu>
2015-02-25 23:23:42 +0100

    debut interract

--- app/js/nav-voo.js
+++ app/js/nav-voo.js
@@ -1,44 +1,44 @@
-(function($,window){
-  $(function(){
-    setHandlers();
-
+(function($) {
+  $(function() {
+      var menu = Menu('nav');
   });
+  function Menu(ele){
+    var baseElement = $('.'+ele);;
+    function init(){
+      console.log('init');
+      initHandlers();
 
-
-  function setHandlers(){
-    $('.high-menu-item').on('mouseenter', mouseEnterHandler);
-    $('.sub-menu-item').on('click', clickHandler);
-    $('.sub-menu-active .li .a').on('click', clickSubMenuHandler);
-  }
-
-  function mouseEnterHandler(event){
-  	var link = $(this).attr('href');
-    var test = $(this).attr('data-button-menu');
-    if(test != undefined){
-    	$('.sub-menu-'+link).show();
-    }else{
-    	$('.high-menu-item').off('mouseenter', mouseEnterHandler);
-    }	
-  }
-
-  function clickHandler(event){
-    var test = $(this).attr('data-button-menu');
-    event.preventDefault();
-    if(test != undefined){
-    	$('.high-menu').hide();
-    	$('.logo-sub-menu').show();
-    	$('.back-sub-menu').show();
-    	// $(this).addClass('sub-menu-active');
-    	$('.sub-menu-products').show().addClass('sub-menu-active').css({'top' : '0','background-color' : '#ccc'});
     }
+
+    function initHandlers()
+    {
+      console.log('initHandlers');
+      console.log(baseElement);
+      $('a[TPK-MENU="global-menu-button"]').on('click',_clickHandler);
+      $('a[TPK-MENU="products-menu-button"]').on('click',_clickHandler);
+
+    }
+
+
+    function _clickHandler(e){
+      e.preventDefault();
+      eval('interract_'+$(this).attr('TPK-ID'))();
+    }
+
+
+    function interract_products(){
+      $(".nav").hide();
+      $(".sub-menu-products").show();
+    }
+
+    function interract_packs(){
+      $(".sub-menu-products-packs").show();
+    }
+
+
+    init();
+    return {
+
+    };
   }
-
-  function clickSubMenuHandler(event){
-  	event.preventDefault();
-  	alert('Yooooooo');
-  }
-
-
-})(jQuery,window);
-
-    // if(link.indexOf("http://") != -1)window.open(link);
+})(jQuery);
