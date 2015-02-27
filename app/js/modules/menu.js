2898347fc942356050592d86c9acb35e6cf45568
Guillaume <Guillaume@MacBook-Pro-de-guillaume.local>
2015-02-27 16:31:01 +0100

    maj logo

--- app/js/modules/menu.js
+++ app/js/modules/menu.js
@@ -7,12 +7,11 @@
 
       initHandlers();
 
-
     }
 
     function initHandlers() {
       // log.info('initHandlers');
-      $('.button-products').on('click', showProductsMenu).on('mouseover', showProductsMenu)
+      $('.button-products').on('click', showProductsMenu).on('mouseover', showProductsMenu);
     }
 
     function showProductsMenu(e) {
@@ -20,39 +19,33 @@
       e.stopPropagation();
       $('.sub-menu-products').fadeIn();
       $('.sub-menu-products').on('mouseleave', hideProductsMenu);
-      $('.sub-menu-products-item').on('click',switchSubMenu);
+      $('.sub-menu-products-item').on('click', switchSubMenu);
     }
-
 
     function hideProductsMenu(e) {
       e.preventDefault();
       $('.sub-menu-products').fadeOut();
-
     }
 
     function switchSubMenu(e){
       e.preventDefault();
-      console.log(e);
       $('.sub-menu-products').css({'top':'0', 'background-color':'#ccc'});
       $('.sub-menu-products').off('mouseleave', hideProductsMenu);
       $('.sub-menu-products-item').on('mouseover', showSubMenu);
-      $('#truc').on('mouseleave', hideSubMenu);
+      $('.hidden-item').show();
+      $('#big-nav').on('mouseleave', hideSubMenu);
     }
 
     function showSubMenu(e){
       e.preventDefault();
       //console.log(e);
-      if($('.sub-menu-products-'+ $(this).attr('TPK-ID')+':hidden')){
-        $('.testtruc').fadeOut();
-        $('.sub-menu-products-'+ $(this).attr('TPK-ID')).fadeIn();
-      }
-      
+      $('.sub-sub-menu').hide();
+      $('.sub-menu-products-'+ $(this).attr('TPK-ID')).fadeIn();
     }
 
     function hideSubMenu(e){
       e.preventDefault;
-      //$('.sub-menu-products-'+ $(this).attr('TPK-ID')).fadeOut();
-      $('.testtruc').fadeOut();
+      $('.sub-menu-products-'+ $(this).attr('TPK-ID')).fadeOut();
     }
 
     init();
